e825fa6a9aa5bcd947fc50368043636e61a9faca
Rauf <161576550+Raufd012@users.noreply.github.com>
2024-04-05 03:41:32 +0400

    initial commit

---
+++ 05.04.2003/05.04.2003/Models/Animals.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05._04._2003.Models
+{
+    internal class Animals
+    
+        {
+            public string Name { get; set; }
+        public abstract void Eat();
+        public virtual void Grow()
+            {
+                Console.WriteLine("Grew");
+            }
+        }
+        abstract class Bird : Animals
+        {
+            public virtual void Fly()
+            {
+                Console.WriteLine("Fly as Bird");
+            }
+        }
+        class Parrot : Bird
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("Eat as Parrot");
+            }
+
+            public override void Fly()
+            {
+                Console.WriteLine("Fly as Parrot");
+            }
+
+            public void Talk()
+            {
+                Console.WriteLine("Cici kush");
+            }
+            public override void Grow()
+            {
+                base.Grow();
+            }
+        }
+        class Eagle : Bird
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("Eat as Eagle");
+            }
+        }
+        class Duck : Bird, ISwim
+    {
+            public override void Eat()
+            {
+                Console.WriteLine("Eat as Duck");
+            }
+
+        }
+        class Penguin : Bird
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("Eat as Penguin");
+            }
+            public override void Fly()
+            {
+                Console.WriteLine("Sorry penguin cannot fly");
+            }
+        }
+        abstract class Fish : Animals
+        {
+            public void Swim()
+            {
+                Console.WriteLine("Swim");
+            }
+        }
+        class Shark : Fish
+        {
+            public override void Eat()
+            {
+                Console.WriteLine("Eat as Shark");
+            }
+            public override void Grow()
+            {
+                Console.WriteLine("Grew as Shark");
+            }
+        }
+    }
+
---
+++ 05.04.2003/05.04.2003/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _05._04._2003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+
+
+        {
+            //task1
+
+            //    StudentRecord std1 = new StudentRecord()
+            //    {
+            //        Name = "Ulvi",
+            //        Surname = "Abdullazada",
+            //        Age = 24,
+            //        Group = "AB106",
+            //        Uni = "AzTU"
+            //    };
+            //    StudentRecord std2 = std1 with { Name = "Rauf", Surname = "Dostuyev" };
+            //    Console.WriteLine(std1);
+            //    Console.WriteLine(std2);
+            //}
+            //record StudentRecord(string Name = "XXX", string Surname = "YYY", int Age = 0, string Group = "", string Uni = "");
+
+
+            //animal task
+
+
+            Bird quw = new Parrot();
+            Shark akula = new Shark() { Name = "Elxan" };
+            Eagle eagle = new Eagle() { Name = "Sahib" };
+            Animal[] animals = { quw, akula, eagle };
+
+            Bird[] birds = { quw, eagle, new Penguin() };
+            quw.Eat();
+
+            quw.Fly();
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.ToString());
+            }
+            quw.Talk();
+            Console.WriteLine(quw);
+            quw.Fly();
+        }
+    }
+}
